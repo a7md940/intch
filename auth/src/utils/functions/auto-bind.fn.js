135d6d7cf441347c57747d870677713585dfd2dd
a7mdRadwan <ahmed.radwan@in3.cloud>
2020-12-04 22:37:57 +0200

    create autobind function to bind classes methods to propper context.

---
+++ auth/src/utils/functions/auto-bind.fn.js
@@ -0,0 +1,28 @@
+/**
+ * Bind functions context
+ * @param {any} klass Class instance `{this} keyword` to bind the contrllers function contexts
+ */
+module.exports = (klass) => {
+    let isFunction = (v) => typeof v === 'function';
+    let proto = Object.getPrototypeOf(klass);
+    let propertyNames = Object.getOwnPropertyNames(proto)
+    for (var _iterator = propertyNames, _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator](); ;) {
+        var _ref;
+
+        if (_isArray) {
+            if (_i >= _iterator.length) break;
+            _ref = _iterator[_i++];
+        } else {
+            _i = _iterator.next();
+            if (_i.done) break;
+            _ref = _i.value;
+        }
+
+        var name = _ref;
+
+        var value = proto[name];
+        if (isFunction(value)) {
+            klass[name] = proto[name].bind(klass);
+        }
+    }
+}
