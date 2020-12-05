e54323b771e0ca97292076f0689c2827b2d71697
a7mdRadwan <ahmed.radwan@in3.cloud>
2020-12-06 01:32:10 +0200

    setup web app socket

--- web-app/src/app/services/socket.service.ts
+++ web-app/src/app/services/socket.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
+import { filter, map } from 'rxjs/operators';
+
+import { AuthService } from './auth.service';
+import { UserAuth } from '../models/auth/user-auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +13,25 @@ export class SocketService {
   private _config = {
     socketChat: ''
   };
-  io: Socket;
-  constructor() {
-    this.io = io('', { path: '/realtime', transports: ['websocket'] });
-    console.log('socket initialized!');
+  socketClient!: Socket;
+
+  constructor(private _authService: AuthService) {
+    this._authService.getUserAuth()
+      .pipe(filter(x => x != null), map(x => x as UserAuth))
+      .subscribe((userAuth) => {
+        this.socketClient = io(`/?auth=${userAuth.token}`, { path: '/realtime', transports: ['websocket'] });
+      });
+
+  }
+  sendMessage(event: string, payload: any): void {
+    this.socketClient.emit(event, payload);
+  }
+
+  listen<T>(event: string): Observable<T> {
+    return new Observable((observer) => {
+      this.socketClient.on(event, (message: T) => {
+        observer.next(message);
+      });
+    });
   }
 }
